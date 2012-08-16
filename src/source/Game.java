566db5ec435432f25589a0664705c595df8910bd
martinm2w@gmail.com <martinm2w@gmail.com@debfd88d-ddd2-3143-bf95-9689f3f83fbb>
2012-08-16 03:38:38 +0000

    basic

---
+++ src/source/Game.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package source;
+
+/**
+ *
+ * @author ruobo 
+ * @lastupdate 8/15/12 11:33 PM
+ */
+public class Game {
+    
+}
