9c9f38fdbdec5d847d1f5a12a258bbf232832010
martinm2w@gmail.com <martinm2w@gmail.com@debfd88d-ddd2-3143-bf95-9689f3f83fbb>
2012-08-16 03:38:38 +0000

    basic

---
+++ trunk/src/source/Achievements.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package source;
+
+/**
+ *
+ * @author ruobo
+ * @lastupdate 8/15/12 11:32 PM
+ */
+public class Achievements {
+    
+}
---
+++ trunk/src/source/Player.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package source;
+
+/**
+ *
+ * @author ruobo
+ * @lastupdate 8/15/12 11:33 PM
+ */
+public class Player {
+    
+    
+    
+//==========================getters & setters===================================
+    /**
+     * @return the Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * @param Name the Name to set
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    /**
+     * @return the Email
+     */
+    public String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param Email the Email to set
+     */
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+    
+    
+    /**
+     * @return the DateOfBirth
+     */
+    public String getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    /**
+     * @param DateOfBirth the DateOfBirth to set
+     */
+    public void setDateOfBirth(String DateOfBirth) {
+        this.DateOfBirth = DateOfBirth;
+    }
+
+    
+//===========================instance vars======================================
+    private String Name;
+    private String DateOfBirth;
+    private String Email;
+
+
+ 
+}
