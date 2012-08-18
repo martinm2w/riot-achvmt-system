58a485f97070038c182eea890f9f47527100d814
martinm2w@gmail.com <martinm2w@gmail.com@debfd88d-ddd2-3143-bf95-9689f3f83fbb>
2012-08-18 02:30:58 +0000

    current game stats structure

--- trunk/src/util/HistoryStats.java
+++ trunk/src/util/HistoryStats.java
@@ -1,5 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package util;
 
+/**
+ *
+ * @author ruobo
+ * @lastupdate Aug 17, 2012 
+ */
 public class HistoryStats {
-
+//===============================constructor====================================
+//==================================utils=======================================
+//==============================instance vars===================================
+//============================setters & getters=================================
 }
