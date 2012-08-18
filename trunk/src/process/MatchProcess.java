57a0d672e171409cc073e175daf5fadb90ce6f32
martinm2w@gmail.com <martinm2w@gmail.com@debfd88d-ddd2-3143-bf95-9689f3f83fbb>
2012-08-18 01:23:45 +0000

    structure

--- trunk/src/process/MatchProcess.java
+++ trunk/src/process/MatchProcess.java
@@ -2,13 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package entities;
+package process;
 
 /**
  *
  * @author ruobo
- * @lastupdate 8/15/12 11:32 PM
  */
-public class Achievements {
+public class MatchProcess {
     
 }
