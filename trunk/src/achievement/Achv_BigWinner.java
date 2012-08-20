83cc90ae3cf316a0d50c741bf48f42c2595e88bd
martinm2w@gmail.com <martinm2w@gmail.com@debfd88d-ddd2-3143-bf95-9689f3f83fbb>
2012-08-20 20:13:05 +0000

    update from work

--- trunk/src/achievement/Achv_BigWinner.java
+++ trunk/src/achievement/Achv_BigWinner.java
@@ -20,15 +20,24 @@ public class Achv_BigWinner extends Achievement{
 //================================public========================================    
     @Override
     public void checkAchvCriteria(Player player) {
+        if(player ==null) return;
         int totalWin = player.getHisStats().getTotal_wins();
         if(totalWin>=200){
             super.setFulfilled(true);
         }
     }
 
+    /**
+     * 
+     * @param player 
+     * @lastupdate 8/20/12 3:37 PM
+     */
     @Override
     public void rewardIfFullfilled(Player player) {
-        player.setInfluencePoints(player.getInfluencePoints()+100);
+        if(player ==null) return;
+        if(super.isFulfilled()){
+            player.setInfluencePoints(player.getInfluencePoints()+100);
+        }
     }
 //=================================private======================================
 //==============================instance vars===================================
