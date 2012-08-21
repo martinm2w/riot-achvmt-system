88279493c45932320a78bd38a5c2b455db3d1491
martinm2w@gmail.com <martinm2w@gmail.com@debfd88d-ddd2-3143-bf95-9689f3f83fbb>
2012-08-21 02:55:36 +0000

    3rd day

--- src/heroes/Hero.java
+++ src/heroes/Hero.java
@@ -13,16 +13,103 @@ import java.util.ArrayList;
  * @author ruobo
  * @lastupdate Aug 19, 2012 
  */
-public abstract class Hero {
+public class Hero {
 //===============================constructor====================================
+    public Hero(String heroName){
+        this.setHeroName(heroName);
+    }
 //==================================public======================================
 //=================================private======================================
 //==============================instance vars===================================
     private String heroName;
-    private String lore;
-    private String tipsAndItems;
-    private ArrayList<Ability> abilities;
-    private BufferedImage art;
-    private BufferedImage portait;
+    private String lore = "some lore of " + heroName;
+    private String tipsAndItems = "use me wisely";
+    private ArrayList<Ability> abilities;//omitted for simplicity
+    private BufferedImage art;//omitted for simplicity
+    private BufferedImage portait;//omitted for simplicity
 //============================setters & getters=================================
+
+    /**
+     * @return the heroName
+     */
+    public String getHeroName() {
+        return heroName;
+    }
+
+    /**
+     * @param heroName the heroName to set
+     */
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    /**
+     * @return the lore
+     */
+    public String getLore() {
+        return lore;
+    }
+
+    /**
+     * @param lore the lore to set
+     */
+    public void setLore(String lore) {
+        this.lore = lore;
+    }
+
+    /**
+     * @return the tipsAndItems
+     */
+    public String getTipsAndItems() {
+        return tipsAndItems;
+    }
+
+    /**
+     * @param tipsAndItems the tipsAndItems to set
+     */
+    public void setTipsAndItems(String tipsAndItems) {
+        this.tipsAndItems = tipsAndItems;
+    }
+
+    /**
+     * @return the abilities
+     */
+    public ArrayList<Ability> getAbilities() {
+        return abilities;
+    }
+
+    /**
+     * @param abilities the abilities to set
+     */
+    public void setAbilities(ArrayList<Ability> abilities) {
+        this.abilities = abilities;
+    }
+
+    /**
+     * @return the art
+     */
+    public BufferedImage getArt() {
+        return art;
+    }
+
+    /**
+     * @param art the art to set
+     */
+    public void setArt(BufferedImage art) {
+        this.art = art;
+    }
+
+    /**
+     * @return the portait
+     */
+    public BufferedImage getPortait() {
+        return portait;
+    }
+
+    /**
+     * @param portait the portait to set
+     */
+    public void setPortait(BufferedImage portait) {
+        this.portait = portait;
+    }
 }
